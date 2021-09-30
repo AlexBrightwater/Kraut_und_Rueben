243128db8e3a87cb4575438a44c98152efa3c0c7
Alexander <90156402+AlexBrightwater@users.noreply.github.com>
2021-09-30 13:41:33 +0200

    added frontend files (not working 100% atm)

---
+++ frontend/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/indexx.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Kraut und Rueben</title>
+</head>
+
+<body>
+    <h1 class="mainheader">Kraut und Rueben</h1>
+</body>
+
+</html>
