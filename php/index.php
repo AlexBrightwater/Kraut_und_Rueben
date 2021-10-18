7ffc4942df536c4f30255f1040e7aae773978839
Niko Ehlers <niko.ehlers.010@gmail.com>
2021-10-18 15:09:35 +0200

    Made functions more accessible for the frontend

--- php/index.php
+++ php/index.php
@@ -1,13 +1,33 @@
 <?php
     require_once 'conn.php';
     require_once 'functions.php';
+?>
 
-    echo "<h1>Kraut und Rüben</h1>";
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Kraut und Rueben</title>
+    </head>
+    <body>
+        <h1>Kraut und Rueben</h1>
+        <br />
+        <h3>Zutaten</h3>
+        <table>
+            <tr>
+                <th>Zutat</th>
+                <th>Preis</th>
+                <th>Bestand</th>
+            </tr>
+            <?php
+                $ingredients = AllIngredients($conn);
+                while ($row = mysqli_fetch_assoc($ingredients)){
+                    echo "<tr> <td>". $row["zutat_name"] ."</td> <td>". $row["nettopreis_ct"] / 100 ." €</td> <td>". $row["bestand"] ."</td> </tr>";
+                }
+            ?>
+        </table>
+    </body>
+</html>
 
-
-    AllRecipesOfCategory($conn,"Vegetarisch");
-    AllIngredientsOfRecipe($conn,"Caesersalat");
-    AllRecipesWithIngredient($conn, "Zucchini");
-
-
+<?php
     mysqli_close($conn);
+?>
